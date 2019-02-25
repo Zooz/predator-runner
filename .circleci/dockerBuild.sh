d28999802991c540ce2842b3061b4680a11213e6
NivLipetz <31369413+NivLipetz@users.noreply.github.com>
2019-02-25 09:58:29 +0200

    chore(pipeline): push development images into zooz/predator-builds repo (#9)

---
+++ .circleci/dockerBuild.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+if [ "$CIRCLE_BRANCH" != "master" ] ; then
+    export TAG=predator-runner-$CIRCLE_BRANCH
+    export IMAGE=zooz/predator-builds:$TAG
+else
+    export TAG=latest
+    export IMAGE=zooz/predator-runner:$TAG
+fi
+
+echo "Building Docker image $IMAGE on branch: $CIRCLE_BRANCH"
+
+docker build -t $IMAGE .
+
+echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
+
+docker push $IMAGE
