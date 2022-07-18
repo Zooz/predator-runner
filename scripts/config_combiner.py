9005c2e704ce0edb9531390a4aae2bfdb15332a1
Boris Bakshiyev <boris.b@zooz.com>
2022-07-18 08:44:22 +0300

    Test

--- scripts/config_combiner.py
+++ scripts/config_combiner.py
@@ -10,19 +10,14 @@ class MyDumper(yaml.Dumper):
 
 
 sarifFiles = glob.glob('semgrep_rules/*')
-print(sarifFiles)
 allRules = []
 
 for scanConfig in sarifFiles:
     f = open(scanConfig, 'r')
-    print('BORIS: Opened the file:', scanConfig)
     scanConfigData = yaml.safe_load(f)
     allRules += scanConfigData['rules']
-    print('BORIS RULES:\n', allRules)
 
 newConfig = {'rules': allRules}
-print('BORIS: The new config is\n', newConfig)
 outFile = open('semgrep_rules/semgrepRulles.yml', 'w')
 outFile.write(yaml.dump(newConfig, Dumper=MyDumper, default_flow_style=False))
 outFile.close()
-print('Wrote the file')
