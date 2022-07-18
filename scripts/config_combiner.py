6dcf7e6e3e55b33d0ce6cc932eb2faabd52db5fd
Boris Bakshiyev <boris.b@zooz.com>
2022-07-18 08:28:24 +0300

    Test

--- scripts/config_combiner.py
+++ scripts/config_combiner.py
@@ -2,36 +2,11 @@
 
 import yaml
 import glob
-# import json
-# import collections
 
 class MyDumper(yaml.Dumper):
 
     def increase_indent(self, flow=False, indentless=False):
         return super(MyDumper, self).increase_indent(flow, False)
-
-
-# sarifFiles = glob.glob('*.sarif')
-# print(sarifFiles)
-
-# allRuns = []
-# allRules = []
-
-# for report in sarifFiles:
-#     f = open(report, 'r')
-#     data = json.load(f)
-#     if len(data['runs'][0]['results']) > 0:
-#         allRuns += data['runs'][0]['results']
-#         allRules += data['runs'][0]['tool']['driver']['rules']
-#     f.close() 
-
-# if len(allRuns) > 0:
-#     tempReport = open(sarifFiles[0], 'r')
-#     tempData = json.load(tempReport)
-
-#     # tempData.append(allRuns)
-#     print(tempData['runs'][0]['results'])
-#     tempReport.close()
 
 sarifFiles = glob.glob('../semgrep_rules/*')
 allRules = []
